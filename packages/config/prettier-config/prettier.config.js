35210c4510433dda34a17d5252bd2d2a3f3f2f14
FlaSh2493 <flash0211@naver.com>
2025-12-30 23:01:48 +0900

    chore: 누락된 파일 추가

---
+++ packages/config/prettier-config/prettier.config.js
@@ -0,0 +1,8 @@
+export default {
+  $schema: 'https://json.schemastore.org/prettierrc.json',
+  printWidth: 100,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+}
